92903763d43fe5ef504c8066b8ccf34668ce9162
Timothy Chan <timothyqchan@gmail.com>
2024-03-03 21:05:31 -0500

    fix auth jwt_secret

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,21 +6,26 @@ const handleAuthorization = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    throw new UnauthorizedError("Authorization required");
+    throw new UnauthorizedError("Authorization token is missing");
   }
 
   const token = authorization.replace("Bearer ", "");
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(
+      token,
+      NODE_ENV === "production" ? JWT_SECRET : "secret-key",
+    );
   } catch (err) {
-    throw new UnauthorizedError("Authorization required");
+    return next(new UnauthorizedError("Authorization token is incorrect"));
   }
 
   req.user = payload;
 
-  return next();
+  next();
+
+  return null;
 };
 
 module.exports = { handleAuthorization };
